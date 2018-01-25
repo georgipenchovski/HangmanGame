713adf9bed8fcc289a15969bd2c2ae57da8afdba
georgipenchovski <georgipenchovski@gmail.com>
2018-01-25 02:39:33 +0200

    Finished game

--- src/HangmanGame.java
+++ src/HangmanGame.java
@@ -3,34 +3,40 @@ import java.util.Scanner;
 
 public class HangmanGame {
     public static void main(String[] args) {
-        String[] words = {"Айтос", "Аксаково", "Алфатар", "Антоново", "Априлци", "Ардино", "Асеновград", "Ахелой", "Ахтопол",
-                "Балчик", "Банкя", "Банско", "Баня", "Батак", "Батановци", "Белене", "Белица", "Белово", "Белоградчик", "Белослав", "Благоевград",
+        String[] words = {"Айтос", "Аксаково", "Антоново", "Априлци", "Ардино", "Асеновград", "Ахелой", "Ахтопол",
+                "Балчик", "Банкя", "Банско", "Баня", "Батак", "Бяла Слатина", "Белене", "Белица", "Белово", "Белоградчик", "Благоевград",
                 "Бойчиновци", "Болярово", "Борово", "Ботевград", "Брезово", "Брусарци", "Бургас", "Бухово", "Българово", "Бяла",
-                "Варна", "Велинград", "Ветово", "Ветрен", "Видин", "Враца", "Вълчедръм", "Върбица", "Вършец",
-                "Габрово", "Главиница", "Глоджево", "Годеч", "Грамада", "Гулянци", "Гурково", "Гълъбово",
+                "Варна", "Велинград", "Ветово", "Велико Търново", "Видин", "Враца", "Върбица", "Вършец",
+                "Габрово", "Главиница", "Глоджево", "Годеч", "Грамада", "Гурково",
                 "Дебелец", "Девин", "Девня", "Джебел", "Димитровград",
                 "Земен", "Златарица", "Златица", "Златоград",
                 "Ивайловград", "Игнатиево", "Ихтиман",
-                "Каблешково", "Каварна", "Казанлък", "Калофер", "Камено", "Каолиново", "Карлово", "Карнобат", "Каспичан", "Кермен", "Клисура",
-                "Китен", "Кнежа", "Козлодуй", "Койнаре", "Кресна", "Криводол", "Крумовград", "Кубрат", "Куклен", "Кула", "Кърджали", "Кюстендил",
+                "Каварна", "Казанлък", "Калофер", "Камено", "Карлово", "Карнобат", "Каспичан", "Кермен", "Клисура",
+                "Китен", "Кнежа", "Козлодуй", "Койнаре", "Кресна", "Криводол", "Кубрат", "Куклен", "Кула", "Кърджали", "Кюстендил",
                 "Левски", "Ловеч", "Лом", "Луковит", "Лъки", "Любимец", "Лясковец",
                 "Маджарово", "Мартен", "Мелник", "Мездра",
                 "Меричлери", "Мизия", "Момчилград", "Монтана", "Мъглиж",
                 "Неделино", "Несебър", "Николаево", "Никопол",
-                "Обзор", "Омуртаг", "Опака", "Оряхово",
+                "Обзор", "Оряхово",
                 "Павликени", "Пазарджик", "Панагюрище", "Перник", "Перущица", "Петрич",
                 "Разград", "Разлог", "Ракитово", "Раковски", "Рила", "Роман", "Рудозем", "Русе",
-                "Садово", "Самоков", "Сандански", "Свиленград", "Септември", "Силистра", "Симеоновград", "Симитли", "Славяново", "Сливен",
+                "Садово", "Самоков", "Сандански", "Свиленград", "Силистра", "Симитли", "Славяново", "Сливен",
                 "Смолян", "Созопол", "Сопот", "София", "Средец", "Стражица", "Стралджа", "Стрелча",
                 "Твърдица", "Тервел", "Тетевен", "Тополовград", "Троян", "Трън",
                 "Шипка", "Шумен",
                 "Ябланица", "Якоруда", "Ямбол"};
 
         int randomWordNumber = (int) (Math.random() * words.length);
+
+        play(words, randomWordNumber);
+    }
+
+    public static void play(String[] words, int randomWordNumber) {
         char[] enteredLetters = new char[words[randomWordNumber].length()];
         int triesCount = 0;
         boolean wordIsGuessed = false;
-        System.out.println("Познайте думата.");
+
+        System.out.println("Вие започнахте играта \"Бесеница\".");
         System.out.println("    _____ ");
         System.out.println("    |   | ");
         System.out.println("    |     ");
@@ -39,7 +45,6 @@ public class HangmanGame {
         System.out.println("    |     ");
 
         do {
-
             switch (enterLetter(words[randomWordNumber], enteredLetters)) {
                 case 0:
                     triesCount++;
@@ -103,42 +108,55 @@ public class HangmanGame {
 
         }
         while (!wordIsGuessed);
+        if (triesCount != 6) {
+            System.out.print("\nТи победи!");
+        }
         System.out.println("\nДумата е " + "\"" + words[randomWordNumber] + "\"" + ".");
 
     }
 
+    public static boolean isCyrillic(char c) {
+        return Character.UnicodeBlock.CYRILLIC.equals(Character.UnicodeBlock.of(c));
+    }
 
     public static int enterLetter(String word, char[] enteredLetters) {
-
-        System.out.print("Въведете буква на кирилица!");
-
+        word = word.toLowerCase();
+        int emptyPosition = findEmptyPosition(enteredLetters);
+        System.out.print("Познайте думата: ");
         if (!printWord(word, enteredLetters))
             return 3;
-        System.out.print(" > ");
+        System.out.print("\nВъведете буква: ");
         Scanner input = new Scanner(System.in);
-        char userInput = input.nextLine().charAt(0);
-        int emptyPosition = findEmptyPosition(enteredLetters);
+        char userInput = input.nextLine().toLowerCase().charAt(0);
+        if (!isCyrillic(userInput)) {
+            System.out.println("Въведете буква на кирилица!");
+            return 1;
+        }
         if (inEnteredLetters(userInput, enteredLetters)) {
-            System.out.println(userInput + " вече е в думата");
-            return 2;
+            System.out.println("\"" + userInput + "\"" + " вече е в думата");
+            return 1;
         } else if (word.contains(String.valueOf(userInput))) {
             enteredLetters[emptyPosition] = userInput;
             return 1;
         } else {
-            System.out.println(userInput + " я няма в думата.");
+            System.out.println("\"" + userInput + "\"" + " я няма в думата.");
             return 0;
         }
     }
 
     public static boolean printWord(String word, char[] enteredLetters) {
-
         boolean isPrinted = false;
         for (int i = 0; i < word.length(); i++) {
             char letter = word.charAt(i);
-
-            if (inEnteredLetters(letter, enteredLetters))
-                System.out.print(letter);
-            else {
+            if (inEnteredLetters(letter, enteredLetters)) {
+                if (i == 0 || word.charAt(i - 1) == ' ') {
+                    System.out.print(Character.toUpperCase(letter));
+                } else {
+                    System.out.print(letter);
+                }
+            } else if (letter == ' ') {
+                System.out.print(' ');
+            } else {
                 System.out.print('*');
                 isPrinted = true;
             }
@@ -156,5 +174,4 @@ public class HangmanGame {
         return i;
 
     }
-
 }
